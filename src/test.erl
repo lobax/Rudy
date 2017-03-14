ee52b8f4bc6ac4bab882f6a0d439c5532f4095a9
Michael Chlebek <michael.chlebek@gmail.com>
2017-03-14 16:59:12 +0100

    files generated during run are place in out/

--- src/test.erl
+++ src/test.erl
@@ -16,7 +16,7 @@ bench(_,0,_) ->
 bench(N, T, Port) -> 
     Iterations = 5,
     {Time, Lost} = bench_average(N, T, Port, Iterations), 
-    file:write_file("output.dat",  io_lib:fwrite("~p ~p ~p ~p ~p~n", [N, T, Time, Lost, Iterations]), [append]),
+    file:write_file("out/output.dat",  io_lib:fwrite("~p ~p ~p ~p ~p~n", [N, T, Time, Lost, Iterations]), [append]),
     io:format("T: ~p, The execution time is: ~p, lost connections: ~p~n", [T, Time, Lost]),
     bench(N, T-1, Port).
 
